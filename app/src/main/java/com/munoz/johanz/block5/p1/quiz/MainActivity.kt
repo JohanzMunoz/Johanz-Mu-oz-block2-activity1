75046dd2ef6028c395dacc09648efc9623750670
Acer <johanzmunoz007@gmail.com>
2023-12-09 23:26:40 +0800

    commit

--- app/src/main/java/com/munoz/johanz/block5/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/munoz/johanz/block5/p1/quiz/MainActivity.kt
@@ -1,17 +1,41 @@
 package com.munoz.johanz.block5.p1.quiz
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
 
     private lateinit var usernameInput: EditText
     private lateinit var passwordInput: EditText
     private lateinit var loginBtn: Button
+
+    constructor(parcel: Parcel) : this() {
+
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,3 +51,4 @@ class MainActivity : AppCompatActivity() {
             Log.i("Test Credentials", "Username: $username and Password: $password")
         }
     }
+}
